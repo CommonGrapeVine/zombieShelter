2796249c1e1aa08f3334dd0d272f32f0ea39ef13
ramon8 <ramon.fer96@gmail.com>
2020-04-14 18:54:06 +0200

    refactor: refactorized the ControlService

--- src/scene/main.scene.js
+++ src/scene/main.scene.js
@@ -3,26 +3,24 @@ import { gameSize, controlKeys } from "../globals";
 
 import { ControlService } from "../services/control.service";
 class MainScene extends Phaser.Scene {
-    keyD;
-    keyA;
+
     player;
-    controls;
+    controlsService;
+
     preload() {
-        this.load.image("player","assets/player.png")
+        this.load.image("player", "assets/player.png")
     }
+
     create() {
-        this.controls = new ControlService(this, controlKeys).controls;
-        this.matter.world.setBounds(0,0,gameSize.width, gameSize.height);
-        this.player = this.matter.add.sprite(0,0, "player");
+        this.controlsService = new ControlService(this, controlKeys);
+        this.matter.world.setBounds(0, 0, gameSize.width, gameSize.height);
+        this.player = this.matter.add.sprite(0, 0, "player");
         this.player.setFixedRotation(0);
     }
+
     update() {
-        if(this.controls.left.isDown){
-            this.player.setVelocityX(-1);
-        }
-        if(this.controls.right.isDown){
-            this.player.setVelocityX(1);
-        }
+        this.controlsService.isKeyDown('left') && this.player.setVelocityX(-1);
+        this.controlsService.isKeyDown('right') && this.player.setVelocityX(1);
     }
 }
- export const mainScene = new MainScene("mainScene");
+export const mainScene = new MainScene("mainScene");
--- src/services/control.service.js
+++ src/services/control.service.js
@@ -1,8 +1,18 @@
+
 export class ControlService {
+
     controls = {};
+
     constructor(scene, keyboardKeys) {
         Object.keys(keyboardKeys).forEach(key => {
             this.controls[key] = scene.input.keyboard.addKey(keyboardKeys[key]);
         })
+    }
+
+    /** Return if the key is pressed
+     * @param key string that refers to the key
+     */
+    isKeyDown = (key) => {
+        return this.controls[key] && this.controls[key].isDown;
     }
 }
