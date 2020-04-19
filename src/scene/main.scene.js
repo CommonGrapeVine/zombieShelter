8df2efa49426a6362c671dfc261910ef6205bc9b
ramon8 <ramon.fer96@gmail.com>
2020-04-19 19:28:32 +0200

    new map added

--- src/scene/main.scene.js
+++ src/scene/main.scene.js
@@ -10,13 +10,14 @@ class MainScene extends Phaser.Scene {
         this.load.image("weapon", "assets/weapon.png");
         this.load.image("bullet", "assets/bullet.png");
         this.load.tilemapTiledJSON("map", "assets/tiledMap.json");
+        this.load.tilemapTiledJSON("map2", "assets/tileMap2.json");
         this.load.image("tiles", "assets/tile.png");
     }
 
     create() {
         // this.matter.world.setBounds(0, 0, gameSize.width, gameSize.height);
-        const map = this.createMap(this);
         this.player = new Player(this, { x: 50, y: 50 }, 'player', 1);
+        const map = this.createMap(this);
     }
 
     update(time, delta) {
@@ -24,27 +25,19 @@ class MainScene extends Phaser.Scene {
     }
 
     createMap = (scene) => {
-        const map = scene.make.tilemap({ key: 'map' });
-        // console.log(map);
+        const map = scene.make.tilemap({ key: 'map2' });
 
         const tileset = map.addTilesetImage("tile", "tiles");
         const layers = {};
-        // map.layers.forEach((layer) => {
-        //     layers[layer.name] = map.createDynamicLayer(layer.name, tileset, 0, 0);
-        // });
+        map.layers.forEach((layer) => {
+            layers[layer.name] = map.createDynamicLayer(layer.name, tileset, 0, 0);
+            map.setCollisionByProperty({ collides: true });
+            scene.matter.world.convertTilemapLayer(layers[layer.name]);
+        });        
 
-
-        const nubes = map.createDynamicLayer("Nubes", tileset, 0, 0);
-        const solid = map.createDynamicLayer("Solid", tileset, 0, 0);
-        
-        map.setCollisionByProperty({ collides: true });
-        scene.matter.world.convertTilemapLayer(solid);
-
-        const details = map.createDynamicLayer("Details", tileset, 0, 0);
-        
         scene.matter.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         scene.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-        // scene.cameras.main.startFollow(this.player.sprite);
+        scene.cameras.main.startFollow(this.player.sprite);
         scene.cameras.main.setBackgroundColor('#345678');
 
         return map;
